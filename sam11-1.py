324cbc1d012f30ad61e3a67f598eeb3ba1fdd11f
Fedorovich96 <165049752+Fedorovich96@users.noreply.github.com>
2024-05-06 20:53:33 +0500

    Update sam11-1.py

--- sam11-1.py
+++ sam11-1.py
@@ -1,13 +1,12 @@
 def fib(n):
-    a, b = 0, 1
+    a, b = 1, 1
     count = 0
     while count < n:
         yield a
         a, b = b, a + b
         count += 1
 
-
-for number in fib(200):
-    print(number)
-
-
+# Генерируем числа Фибоначчи от 200 до 250
+for number in fib(250):
+    if number >= 200:
+        print(number)
