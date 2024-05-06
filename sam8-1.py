3fadc855879bc9958fd94d3fc335137859746697
Fedorovich96 <165049752+Fedorovich96@users.noreply.github.com>
2024-05-06 19:12:15 +0500

    Update sam8-1.py

--- sam8-1.py
+++ sam8-1.py
@@ -1,1 +1,16 @@
+class Dog:
+    def __init__(self, name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
+
+    def bark(self):
+        return f"{self.name} говорит гав"
+
+my_dog = Dog("Шарик", 5, "Дворняга")
+
+# Взаимодействие с объектом
+print(f"Кличка собаки {my_dog.name}.")
+print(f"{my_dog.name},возраст {my_dog.age} лет.")
+print(my_dog.bark())
 
